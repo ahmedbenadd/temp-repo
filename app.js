dfe243568f6b6700d5a0c8d6f592c46e5ea52dbd
Ahmed Benaddou <benaddouahmed2005@gmail.com>
2025-01-17 14:22:12 +0100

    NEW

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const http = require('http');
-const _ = require('lodash');
-
-const items = [1,[ 2,[ 3, 4, 5, 6]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('HOME PAGE');
-    }
-    if (req.url === '/ahmed') {
-        res.write('AHMED PAGE');
-    }
-    res.end("NO PAGE");
+    res.write("Hello World!");
 });
 
-server.listen(3000);
+server.listen(5000, () => {
+    console.log('Server is listening on http://localhost:5000');
+});
